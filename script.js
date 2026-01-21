7918487568e8067eb4ba2a9f78831ab04c8374b9
f20250752-dev <f20250752@hyderabad.bits-pilani.ac.in>
2026-01-21 11:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,68 @@ const loginDiv = document.getElementById("login");
 const dashboardDiv = document.getElementById("dashboard");
 const tasksDiv = document.getElementById("tasks");
 
+const resetBtn = document.getElementById("resetBtn");
+
+resetBtn.addEventListener("click", function () {
+  if (confirm("Reset all tasks?")) {
+    localStorage.removeItem("tasks");
+    location.reload();
+  }
+});
+
+// --------- TASK STORAGE (FAKE BACKEND) ---------
+
+// --------- HELPER FUNCTIONS ---------
+
+function getTasks() {
+  return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function saveTasks(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks(role) {
+  const tasks = getTasks();
+  const taskContainer = document.getElementById("tasksList");
+  taskContainer.innerHTML = "";
+
+  tasks.forEach(task => {
+    const div = document.createElement("div");
+    div.className = "task";
+
+    div.innerHTML = `
+      <p class="${task.status === "done" ? "completed" : ""}">
+        ${task.title}
+      </p>
+      ${
+        role === "Developer" && task.status !== "done"
+          ? `<button onclick="completeTask(${task.id})">Mark as Done</button>`
+          : ""
+      }
+    `;
+
+    taskContainer.appendChild(div);
+  });
+}
+
+// --------- ASSIGN TASK (LEAD) ---------
+assignTaskBtn.addEventListener("click", function () {
+  const taskTitle = prompt("Enter task name:");
+  if (!taskTitle) return;
+
+  const tasks = getTasks();
+
+  tasks.push({
+    id: Date.now(),
+    title: taskTitle,
+    status: "pending"
+  });
+
+  saveTasks(tasks);
+  renderTasks("Lead");
+});
+
 // Role selection
 const roleSelect = document.getElementById("roleSelect");
 const roleSection = document.getElementById("roleSection");
@@ -25,11 +87,16 @@ continueBtn.addEventListener("click", function () {
   dashboardDiv.style.display = "block";
   tasksDiv.style.display = "block";
 
+  dashboardDiv.classList.add("fade-in");
+tasksDiv.classList.add("fade-in");
+
   // RBAC
   if (assignTaskBtn) {
     assignTaskBtn.style.display =
       role === "Developer" ? "none" : "block";
   }
+  renderTasks(role);
+
 });
 
 // --------- LOGOUT ---------
@@ -72,4 +139,3 @@ doneButtons.forEach((btn) => {
     alert("Notification: Task marked as completed");
   });
 });
-
